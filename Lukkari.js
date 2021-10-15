0c0e6c0be6628d21abff7d89441d16da94d67113
Tiia-95 <lehmonen.tiia@gmail.com>
2021-10-15 10:09:09 +0300

    Lomakkeen ja ulkoasun tekoa

--- Lukkari.js
+++ Lukkari.js
@@ -1,3 +1,12 @@
-function omaFunktio (){
-    alert("Kukkuu");
+function myFunction(tyyppi) {
+    document.getElementById("paaAihe").value = tyyppi;
+    alert (tyyppi);
+  }
+
+
+
+function tarkastus (){
+    var sivuAihe = document.getElementById("aihe").value;
+    alert (sivuAihe);
 }
+
